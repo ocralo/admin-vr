fd3b400c884f8847ee022246fc410587e2a23452
ocralo <ocralo@gmail.com>
2020-07-22 01:37:40 -0500

    Updates

---
+++ precache-manifest.a9143b38436c39b5bdf29c7a23384d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca1a0a89fcd8045d3585dd741b3b4a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15fb6faf51ce2bcc4cf4",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0220bdb0a433dd3fdf4f",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "15fb6faf51ce2bcc4cf4",
+    "url": "./static/js/2.4c683670.chunk.js"
+  },
+  {
+    "revision": "83cf22677282ef599fd4eb17fb28d692",
+    "url": "./static/js/2.4c683670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0220bdb0a433dd3fdf4f",
+    "url": "./static/js/main.82b484d6.chunk.js"
+  },
+  {
+    "revision": "fc5fc6c3961f57201e3a",
+    "url": "./static/js/runtime-main.6dc1a488.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a94ef884908a82d06abd5dae37351fa.js"
+  "./precache-manifest.a9143b38436c39b5bdf29c7a23384d87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
